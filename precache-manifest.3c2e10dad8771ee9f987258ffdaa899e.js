8c2ec2f78c01314383b0a51e185343df5e5e7fdd
Leo Porto <leonardoreisporto@gmail.com>
2020-06-11 07:55:03 -0300

    Updates

---
+++ precache-manifest.3c2e10dad8771ee9f987258ffdaa899e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8288050b91b141a560e1ec633b2e2559",
+    "url": "/brilha-brilha/index.html"
+  },
+  {
+    "revision": "09410e69275b35fe76a2",
+    "url": "/brilha-brilha/static/js/2.6617b313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brilha-brilha/static/js/2.6617b313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34aa5a41fc5f8e3fc066",
+    "url": "/brilha-brilha/static/js/main.9d5375c0.chunk.js"
+  },
+  {
+    "revision": "e67916d2cb52ebf8c8d0",
+    "url": "/brilha-brilha/static/js/runtime-main.c0ea9052.js"
+  }
+]);
